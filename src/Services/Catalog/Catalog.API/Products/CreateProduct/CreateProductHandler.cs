f56b73876d5de0009f408bec0fd1cd21807cb32e
AmrMohamedNabih <amrnibo123@gmail.com>
2025-02-22 18:40:06 +0200

    Adding catalog services

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommend(string Name ,List<string> Categoury , string Description , string ImageFile , decimal Price ) : ICommend<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommendHandler(IDocumentSession session) : ICommendHandler<CreateProductCommend, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommend request, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = request.Name,
+                Category = request.Categoury,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price,
+
+            };
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
